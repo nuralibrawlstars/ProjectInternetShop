99f04535a731ffff2489e9f0e4a64f2009a68328
Venera_Islamova <vib13venera@gmail.com>
2025-05-22 12:57:44 +0500

    remove node_modules

---
+++ index.js
@@ -0,0 +1,40 @@
+const dotenv = require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = 8000;
+// const { router: productsRouter } = require('./app/products');
+// const categories = require('./app/categories');
+// const users = require('./app/users');
+// const admin = require('./app/admin');
+const cors = require('cors');
+const mongoose = require('mongoose');
+// const { swaggerUi, swaggerDocument } = require('./swagger');
+
+async function start() {
+  await mongoose.connect('mongodb://localhost:27017/shop');
+  app.use(cors());
+  app.use(express.static('public'));
+  app.use(express.json());
+  // app.use('/products', productsRouter);
+  // app.use('/categories', categories);
+  // app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+  // app.use('/users', users);
+  // app.use('/admin', admin);
+
+  app.listen(port, () => {
+    console.log(`Server started on ${port} port!`);
+  });
+
+  const gracefullShutdown = async () => {
+    console.log('Shutdown gracefully...');
+    await mongoose.disconnect;
+    process.exit(0);
+  };
+  process.on('SIGINT', gracefullShutdown);
+  process.on('SIGTERM', gracefullShutdown);
+}
+
+start().catch(err => {
+  console.error('Failed to start application:', err);
+  process.exit(1);
+});
